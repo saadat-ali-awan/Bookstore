5c61c42c61e8483d55327c2e265749717a05e887
Saadat123456 <saadatlucky313@gmail.com>
2022-03-26 02:56:05 +0500

    List All The Books With a form to Add the book

--- src/pages/books.jsx
+++ src/pages/books.jsx
@@ -1,6 +1,38 @@
+import AddBookForm from '../components/AddBookForm';
+import Book from '../components/Book';
+
 function Books() {
+  const list = [
+    {
+      key: 'Item1',
+      title: 'Book 1',
+      author: 'Author 1',
+    },
+    {
+      key: 'Item2',
+      title: 'Book 2',
+      author: 'Author 1',
+    },
+    {
+      key: 'Item3',
+      title: 'Book 3',
+      author: 'Author 2',
+    },
+  ];
   return (
-    <div>Books</div>
+    <div>
+      <ul>
+        {
+          list.map((book) => (
+            <li key={book.key}>
+              <Book title={`${book.title}`} author={`${book.author}`} />
+              <button type="button">Remove</button>
+            </li>
+          ))
+        }
+      </ul>
+      <AddBookForm />
+    </div>
   );
 }
 
