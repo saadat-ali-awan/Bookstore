a47aebe71e734794c6a9bfeb329ecd9c8f80a1a4
Saadat123456 <saadatlucky313@gmail.com>
2022-03-30 05:25:36 +0500

    Add redux check status logic to Categories Page

--- src/pages/categories.jsx
+++ src/pages/categories.jsx
@@ -1,8 +1,17 @@
+import { useDispatch } from 'react-redux';
+import { checkStatus } from '../redux/categories/categories';
+
 function Categories() {
+  const dispatch = useDispatch();
+
+  function checkStatusHandler() {
+    dispatch(checkStatus());
+  }
+
   return (
     <div>
       <div>Categories</div>
-      <button type="button">Check Status</button>
+      <button type="button" onClick={checkStatusHandler}>Check Status</button>
     </div>
   );
 }
