fad49f05a5e23774f6e179832a1367176e06b720
Saadat123456 <saadatlucky313@gmail.com>
2022-03-30 07:24:31 +0500

    Add Category as prop for book component

--- src/pages/books.jsx
+++ src/pages/books.jsx
@@ -1,13 +1,17 @@
+import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import AddBookForm from '../components/AddBookForm';
 import Book from '../components/Book';
-import { removeBook } from '../redux/books/books';
+import { getBooks, removeBook } from '../redux/books/books';
 
 function Books() {
   const list = useSelector((state) => state.booksReducer);
 
   const dispatch = useDispatch();
 
+  useEffect(() => {
+    dispatch(getBooks());
+  }, []);
   const removeBookFromStore = (id) => {
     dispatch(removeBook(id));
   };
@@ -18,7 +22,7 @@ function Books() {
         {
           list.map((book) => (
             <li key={`item-${book.id}`}>
-              <Book title={`${book.name}`} author={`${book.author}`} />
+              <Book title={`${book.name}`} author={`${book.author}`} category={`${book.category}`} />
               <button type="button" onClick={() => { removeBookFromStore(book.id); }}>Remove</button>
             </li>
           ))
