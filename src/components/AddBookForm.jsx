907dd3e243e6a38efaf7fc2cbd06903d5f5c8abd
Saadat123456 <saadatlucky313@gmail.com>
2022-03-30 05:19:49 +0500

    Add Redux logic to add book form

--- src/components/AddBookForm.jsx
+++ src/components/AddBookForm.jsx
@@ -1,10 +1,16 @@
 import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import uniqid from 'uniqid';
+import { addBook } from '../redux/books/books';
 
 function AddBookForm() {
   const [state, setState] = useState({
     title: '',
     author: '',
   });
+
+  const dispatch = useDispatch();
+  const list = useSelector((state) => state.booksReducer);
 
   function handleChange(e) {
     const { id } = e.target;
@@ -13,6 +19,24 @@ function AddBookForm() {
       [id]: e.target.value,
     });
   }
+
+  function getUniqueId() {
+    const id = uniqid();
+    if (list.filter((book) => book.id === id).length === 0) {
+      return id;
+    }
+    return getUniqueId();
+  }
+
+  const addBookToStore = () => {
+    const id = getUniqueId();
+    dispatch(addBook(state.title, state.author, id));
+    setState({
+      title: '',
+      author: '',
+    });
+  };
+
   return (
     <form>
       <label htmlFor="title">
@@ -23,7 +47,7 @@ function AddBookForm() {
         Author:
         <input type="text" id="author" name="author" value={state.author} onChange={handleChange} />
       </label>
-      <button type="submit">Add Book</button>
+      <button type="button" onClick={addBookToStore}>Add Book</button>
     </form>
   );
 }
