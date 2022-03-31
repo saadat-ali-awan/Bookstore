6b43c23f8e4cb2eff865e99a1beef32e29aaf56f
Saadat123456 <saadatlucky313@gmail.com>
2022-03-31 06:00:19 +0500

    Add Styles to Categories Page

--- src/pages/categories.jsx
+++ src/pages/categories.jsx
@@ -1,17 +1,20 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { checkStatus } from '../redux/categories/categories';
+import styles from './categories.module.css';
 
 function Categories() {
   const dispatch = useDispatch();
+  const status = useSelector((state) => state.statusReducer);
 
   function checkStatusHandler() {
     dispatch(checkStatus());
   }
 
   return (
-    <div>
-      <div>Categories</div>
-      <button type="button" onClick={checkStatusHandler}>Check Status</button>
+    <div className={styles['categories-section']}>
+      <div className={styles.heading}>Categories</div>
+      <div className={styles.status}>{status}</div>
+      <button className={styles['check-status']} type="button" onClick={checkStatusHandler}>Check Status</button>
     </div>
   );
 }
